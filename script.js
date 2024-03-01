7f847df3fd0fbea031a2080e62ace5311ebc3401
Vika <112762182+tosha-vl@users.noreply.github.com>
2024-03-01 20:30:17 +0200

    slick

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.slick-slider').slick({
+        centerMode: true,
+        dots:true,
+        centerPadding: '10px',
+        slidesToShow: 1,
+        variableWidth: true,
+        adaptiveHeight: true
+      }  
+        
+    );
+  });
